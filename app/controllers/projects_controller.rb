06943b0b10d872c8d632c816793c3e2680d87352
Tejaswini <venigallateja@gmail.com>
2015-07-31 00:22:31 -0400

    ready to commit

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,9 @@
 class ProjectsController < ApplicationController
+	def index
+		@projects = Project.all
+		#raise @projects.inspect
+	end
+	def x
+		@projects = Project.find()
+	end
 end
---
+++ db/migrate/20150729041950_add_columns_to_projects.rb
@@ -0,0 +1,7 @@
+class AddColumnsToProjects < ActiveRecord::Migration
+  def change
+  	add_column :projects, :title, :string
+  	add_column :projects, :description, :text
+  	
+  end
+end
